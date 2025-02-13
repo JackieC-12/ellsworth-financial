309ee8ef140a03fb3ef86f2f54f6fe1ff3772241
JackieC-12 <tealkenn@gmail.com>
2025-02-13 10:45:33 -0600

    Changed telephone number on navbar to clickable link

--- react-vite/src/components/Navigation/ContactUs.jsx
+++ react-vite/src/components/Navigation/ContactUs.jsx
@@ -2,7 +2,9 @@ function ContactUs() {
     return(
         <div className="mx-auto px-2 pt-1 sm:px-6 lg:px-8 bg-gray-500 justify-center">
             <div className="flex mx-auto justify-end max-w-7xl pb-2 px-8">
-                <p className="px-4 text-white hover:text-sky-200 cursor-default">(801) 290-8247</p>
+                <a href="tel:801-290-8247">
+                <p className="px-4 text-white hover:text-sky-200">(801) 290-8247</p>
+                </a>
 
                 <a href='/schedule'>
                 <button className="text-white hover:text-sky-200">Schedule a Meeting</button>
