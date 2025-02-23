5053ec029900252562c89e8f9d556970264c9333
JackieC-12 <tealkenn@gmail.com>
2025-02-23 11:54:18 -0600

    Placeholder content for services

--- react-vite/src/components/Services/AssetManagementPage/AssetManagementPage.jsx
+++ react-vite/src/components/Services/AssetManagementPage/AssetManagementPage.jsx
@@ -1,6 +1,81 @@
 function AssetManagementPage() {
     return (
-        <h1>Welcome to Asset Management Page</h1>
+        <div className="pt-24 sm:pt-32 h-full">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 pb-24">
+          <div className="mx-auto grid max-w-2xl gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div className="max-w-xl text-center sm:text-left">
+                <p className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+                Our Firm
+                </p>
+
+                <p className="mt-6 text-xl/9 text-gray-600">
+                At Ellsworth Financial, we specialize in foundational family finance, offering a range of services designed to protect and grow your wealth.
+                Our team of experienced professionals is committed to providing personalized strategies that align with your financial objectives. We believe in
+                building long-term relationships with our clients, ensuring that your financial plans evolve with your changing needs.
+                </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-neutral-100 sm:py-24">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto grid max-w-2xl gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+              <div className="max-w-xl text-center sm:text-left">
+                <p className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Our Mission</p>
+
+                <p className="mt-6 text-xl/9 text-gray-700">We empower individuals and families to achieve financial independence through innovative and
+                   tax-efficient strategies. We strive to provide education and guidance that enable our clients to make informed decisions about
+                   their financial futures. We envision a future where our clients enjoy financial security and peace of mind, knowing that their wealth
+                  is protected and positioned for growth. Our goal is to be a trusted partner in your financial journey, helping
+                  you navigate the complexities of wealth management with confidence.
+                </p>
+              </div>
+
+              <div className="max-w-xl pl-4">
+              <p className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl text-center sm:text-left">What We Value</p>
+              <ul className=" pl-4 mt-6 list-disc text-lg/8 text-gray-700">
+                  <li>
+                    <p className="text-pretty text-gray-900 text-xl font-semibold ">Integrity</p>
+                    <p className="py-4">We uphold the highest standards of honesty and transparency in all our interactions.</p>
+                  </li>
+                  <li>
+                    <p className="text-pretty text-gray-900 text-xl font-semibold ">Client-Centricity</p>
+                    <p className="py-4">Your financial well-being is our top priority. We tailor our services to meet your specific needs and goals.</p>
+                  </li>
+                  <li>
+                    <p className="text-pretty text-gray-900 text-xl font-semibold ">Innovation</p>
+                    <p className="py-4">We leverage cutting-edge financial strategies to provide effective solutions for our clients.</p>
+                  </li>
+                  <li>
+                    <p className="text-pretty text-gray-900 text-xl font-semibold ">Education</p>
+                    <p className="py-4">We believe in empowering our clients with knowledge, enabling them to make informed financial decisions.</p>
+                  </li>
+                </ul>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 py-24">
+          <div className="mx-auto max-w-2xl gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none">
+            <div className="text-center">
+              <p className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Our Approach</p>
+
+              <p className="mt-6 text-xl/9 text-gray-600"> Our goal is not only to position you for success in achieving your financial
+                  objectives but also to provide you with a comprehensive investment plan
+                  that you can feel confident in, even during the inevitable market
+                  fluctuations. We accomplish this through an evidence-based strategy
+                  focused on smart diversification, low investment costs, and risk
+                  minimization. Together, we create a tailored investment strategy designed
+                  to meet your unique goals. Once the strategy is in place, we continue to
+                  monitor and fine-tune your portfolio with active management, rebalancing,
+                  and tax-loss harvesting as needed to keep it aligned with your objectives.</p>
+            </div>
+          </div>
+        </div>
+    </div>
     )
 }
 
